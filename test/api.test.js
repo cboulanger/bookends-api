986f9c390ffe9648c8947c5c15d05b20e0d6d7dd
Christian M. Boulanger <info@bibliograph.org>
2018-07-28 23:45:38 +0200

    Test environment

--- test/api.test.js
+++ test/api.test.js
@@ -1,12 +1,16 @@
-/* global describe, it */
+/* global describe, it, beforeEach, afterEach */
 
 const bookends = require( __dirname + '/../index');
+const fixture = require( __dirname + '/fixture' );
 const assert = require('assert');
 const util = require('util');
 
-describe('Bookends', async function() {
-  //this.timeout(2000);
 
+describe('Bookends', async function() {
+  this.timeout(20000);
+  before(fixture.before);
+  after(fixture.after);
+  
   it('should report its version number', async () => {
     let version = await bookends.getVersion();
     assert( util.isString(version), "Version is not a string." );
@@ -54,5 +58,5 @@ describe('Bookends', async function() {
       'Foo/foo1',
       'Foo/foo2'
      ]);
-  });    
+  });
 });
---
+++ test/fixture.js
@@ -0,0 +1,19 @@
+const util = require('util');
+const osascript = require('node-osascript');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const bookendsExecutablePath = '/Users/cboulanger/Applications/Bookends.app/Contents/MacOS/Bookends';
+const bookendsLibraryPath = __dirname + '/bookends.bdb';
+const process = require('process');
+
+module.exports = {
+  before : async function() {
+    fs.copyFileSync( bookendsLibraryPath + '.dist', bookendsLibraryPath );
+    this.bookendsProcess = spawn(bookendsExecutablePath, [bookendsLibraryPath]);
+    await new Promise(resolve => setTimeout( () => resolve(), 10000));
+  }, 
+  after: async function() {
+    this.bookendsProcess.kill();
+    fs.unlinkSync(bookendsLibraryPath);
+  }
+};  
